892e4301b846932730c0814d40a491185c60e800
Derek Haller <dhaller.personal@gmail.com>
2018-01-29 04:24:07 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -11,6 +11,10 @@ class Song
     @@all << song
     song
   end
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    @@all << song
   def save
     self.class.all << self
   end
